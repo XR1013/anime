00e53c437113e977972435a0444bc0e785cc7cce
XR1013 <xraptor1013@gmail.com>
2024-09-20 14:51:12 +0900

    parseInt를 통해 문자화되어 있는 숫자를 정수형태의 숫자로 강제변환처리

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*
 forEach
 : 배열, 유사배열을 반복처리하기 위한 내장 함수
+
 Array.forEach((data, idx)=>{
     중괄호 안쪽의 코드블록은 배열의 갯수만큼 반복 처리됨
     해당 코드블록 안쪽에서는 data, idx라는 파라미터로 전달되는 값 활용 가능
@@ -14,9 +15,13 @@ btns.forEach((btn, idx) => {
     //forEach반복문 안쪽에서 반복도는 각 btn요소에 click이벤트 연결
     btn.addEventListener("click", (event) => {
         //각 반복도는 btn요소 클릭시 자동으로 전달되는 event.target
-        //클릭이라는 이벹트가 발생한 바로그 DOM요소를 지칭
-        console.log(event.target);
+        //클릭이라는 이벤트가 발생한 바로 그 DOM요소를 지칭 (event.target)
+        // console.log(event.target);
         //이벤트가 발생한 해당 DOM요소에 지정된 data-pos라는 정보값 가져옴
-        console.log(event.target.getAttribute("data-pos"));
+
+        const targetPos = event.target.getAttribute("data-pos");
+        console.log(targetPos); //'1000' 숫자처럼 보이는 문자값
+        const convertedTargetPos = parseInt(targetPos); //'1000' -> 1000 으로 변환
+        console.log(convertedTargetPos);
     });
 });
